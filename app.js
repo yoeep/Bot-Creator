26c070d3f7e1437d01be93d7acfd4a936df22dd8
yono.ji <18262905661@163.com>
2024-03-22 18:46:05 +0800

    2024/03/22 develop end1

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const messageService = require('./service/kook/kookMessage');
 
 kook.onDirectMessage(data=>{
     console.info("non server direct message");
+    messageService.direct('reply for person non server direct message',data.author_id);
 })
 kook.onGroupMessage(data=>{
     console.info("non server group message");
+    messageService.group('reply for group non server group message',data.target_id);
 })
 kook.onGroupMetMessage(data=>{
     console.info("non server group met message");
+    messageService.group('reply for group non server group met message',data.target_id);
 })
 kook.onMessage(data=>{
     console.info("non server message");
